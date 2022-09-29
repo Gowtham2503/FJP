20202a6176f830a5d5cdfea9726477714f2d78fb
Gowtham2503 <112985091+Gowtham2503@users.noreply.github.com>
2022-09-29 14:16:58 -0700

    Create FrogJumpFRQ.java

---
+++ FrogJumpFRQ.java
@@ -0,0 +1,10 @@
+public class FrogJumpFRQ
+{
+  private int goalDistance;
+  private int maxHops;
+  public FrogJumpFRQ(int goalDistance,int maxHops)
+  {
+    this.goalDistance=gpalDistance;
+    this.maxHops=maxHops;
+  }
+}
