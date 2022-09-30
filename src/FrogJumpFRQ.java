333b42f79975accd3cffd06f674e4bd0d51d2d32
s-gsenthilkumar <s-gsenthilkumar@486M123420.lwsd.k12wa.org>
2022-09-29 23:09:49 -0700

    FRQ Successfully completed!

--- src/FrogJumpFRQ.java
+++ src/FrogJumpFRQ.java
@@ -2,11 +2,56 @@ public class FrogJumpFRQ
 {
   private int goalDistance;
   private int maxHops;
+  
   public FrogJumpFRQ(int goalDistance,int maxHops)
   {
-    this.goalDistance=gpalDistance;
+    this.goalDistance=goalDistance;
     this.maxHops=maxHops;
-    
-    //Work at home!
+  }
+  
+  public int hopDistance()
+  {
+	  int answer=(int)(Math.random()*51-10);
+	  return answer;
+  }
+  
+  
+  public boolean simulate()
+  {
+	  int position=0;
+	  int increment=0;
+	  
+  	for(int x=1;x<=maxHops;x++)
+  	{
+  		increment=hopDistance();
+  		position+=increment;
+  		if(position<0)
+  		{
+  			return false;
+  		}
+  		if(position>=goalDistance)
+  		{
+  			return true;
+  		}
+  	}
+  	return false;
+  }
+  
+  public double runSimulations(int num)
+  {
+	  int success=0;
+	  int fail=0;
+	  for(int x=0;x<num;x++)
+	  {
+		  if(simulate())
+		  {
+			  success++;
+		  }
+		  else
+		  {
+			  fail++;
+		  }
+	  }
+	  return((double)success/num);
   }
 }
