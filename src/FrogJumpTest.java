f0c0f2d5f2fddca29353a79d73e38d5c3dcffb53
s-gsenthilkumar <s-gsenthilkumar@486M123420.lwsd.k12wa.org>
2022-09-30 15:23:35 -0700

    Works Successfully!

--- src/FrogJumpTest.java
+++ src/FrogJumpTest.java
@@ -3,6 +3,6 @@ public class FrogJumpTest
 	public static void main(String[] args) 
 	{
 		FrogJumpFRQ sim=new FrogJumpFRQ(23,4);
-		System.out.println(sim.runSimuklations(100));
+		System.out.println(sim.runSimulations(100));
 	}
 }
